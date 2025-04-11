6cc180a99a02d9f5d8898a3c5ac9d96d3570d772
bennystrange <158541711+bennystrange@users.noreply.github.com>
2025-04-11 18:07:55 -0500

    Update datapath_tb.v

--- datapath_tb.v
+++ datapath_tb.v
@@ -7,6 +7,7 @@ module datapath_tb ();
     DATAPATH U1(
         .clka(clka),
         .clkb(clkb),
+        .state(state),
         .btn0(btn0),
         .btn1(btn1),
         .stop(stop),
@@ -37,10 +38,12 @@ module datapath_tb ();
 
     $dumpfile ("datapath_tb.vcd");
     $dumpvars;
+    state = 2'b00;
+    run_cycles(2);
 
     state = 2'b01;
     btn0 =  0;
-    btn1 =  0
+    btn1 =  0;
     stop =  0;
     run_cycles(2);
 
@@ -75,10 +78,8 @@ module datapath_tb ();
     stop =  1;
     run_cycles(2);
 
-    $display ("clka, \t clkb, \t btn0, \t btn1, \t stop, \t state, \t grid,
-    \t grid[0], \t grid[1], \t grid[2], \t grid[3], \t grid[4], \t grid[5], \t grid[6], \t grid[7],
-    \t grid[8], \t grid[9], \t grid[10], \t grid[11], \t grid[12], \t grid[13], \t grid[14], \t grid[15],
-    \t grid[16], \t grid[17], \t grid[18], \t grid[19], \t grid[20], \t grid[21], \t grid[22], \t grid[23]");
+    $display ("clka, \t clkb, \t btn0, \t btn1, \t stop, \t state, \t grid, \t grid[0], \t grid[1], \t grid[2], \t grid[3], \t grid[4], \t grid[5], \t grid[6], \t grid[7], \t grid[8], \t grid[9], \t grid[10], \t grid[11], \t grid[12], \t grid[13], \t grid[14], \t grid[15]");
+
     $stop;
     end
 
