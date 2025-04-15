2cf11bfdaffd6c712413f16d33977a6ab55ef653
bennystrange <158541711+bennystrange@users.noreply.github.com>
2025-04-15 01:05:02 -0500

    Update datapath_tb.v

--- datapath_tb.v
+++ datapath_tb.v
@@ -36,8 +36,8 @@ module datapath_tb ();
     initial
     begin
 
-    $dumpfile ("datapath_tb.vcd");
-    $dumpvars;
+    // $dumpfile ("datapath_tb.vcd");
+    // $dumpvars;
     state = 2'b00;
     run_cycles(2);
 
@@ -68,19 +68,21 @@ module datapath_tb ();
 
     btn0 =  0; // cell 11, 12
     btn1 =  1;
-    run_cycles(1);
+    run_cycles(2);
 
-    state = 2'b10;
     btn0 =  0; // play game
     btn1 =  0;
-    run_cycles(3);
+    run_cycles(2);
+
+    state = 2'b10;
+    run_cycles(10);
 
     stop =  1;
     run_cycles(2);
 
     $display ("clka, \t clkb, \t btn0, \t btn1, \t stop, \t state, \t grid, \t grid[0], \t grid[1], \t grid[2], \t grid[3], \t grid[4], \t grid[5], \t grid[6], \t grid[7], \t grid[8], \t grid[9], \t grid[10], \t grid[11], \t grid[12], \t grid[13], \t grid[14], \t grid[15]");
 
-    $stop;
+    //$stop;
     end
 
 endmodule
