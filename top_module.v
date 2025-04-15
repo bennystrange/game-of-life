8455a9e30ee873d65dd253eb38afe50406905d17
bennystrange <158541711+bennystrange@users.noreply.github.com>
2025-04-15 16:33:10 -0500

    Update top_module.v

--- top_module.v
+++ top_module.v
@@ -1,6 +1,5 @@
-module top_module (in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1, out_cell_idx, out_game_state, out_grid)
+module top_module (in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1, out_game_state, out_grid)
     input wire in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1;
-    output wire [6:0] out_cell_idx;
     output wire [1:0] out_game_state;
     output wire [63:0] out_grid;
 
@@ -11,7 +10,6 @@ module top_module (in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1, 
                         .pp(in_pp),
                         .btn0(in_btn0),
                         .btn1(in_btn1),
-                        .cell_idx(out_cell_idx),
                         .game_state(out_game_state));
     game_of_life_dp dp(.clka(in_clka), 
                         .clkb(in_clkb), 
