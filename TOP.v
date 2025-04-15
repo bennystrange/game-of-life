8f9a434cf0c0cd1a599dd022657b0901c6e9104e
bennystrange <158541711+bennystrange@users.noreply.github.com>
2025-04-15 17:07:32 -0500

    Update TOP.v

--- TOP.v
+++ TOP.v
@@ -1,9 +1,9 @@
-module top_module (in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1, out_game_state, out_grid)
+module TOP (in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1, out_game_state, out_grid)
     input wire in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1;
     output wire [1:0] out_game_state;
     output wire [63:0] out_grid;
 
-    game_of_life_FSM fsm(.clka(in_clka), 
+    FSM U1(.clka(in_clka), 
                         .clkb(in_clkb),
                         .stop(in_stop),
                         .prgm(in_prgm),
@@ -11,7 +11,7 @@ module top_module (in_clka, in_clkb, in_stop, in_prgm, in_pp, in_btn0, in_btn1, 
                         .btn0(in_btn0),
                         .btn1(in_btn1),
                         .game_state(out_game_state));
-    game_of_life_dp dp(.clka(in_clka), 
+    DATAPATH U2(.clka(in_clka), 
                         .clkb(in_clkb), 
                         .state(out_game_state), 
                         .btn0(in_btn0),
